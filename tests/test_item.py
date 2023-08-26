5d42121ca7ba83d4aef90d613d343d99c4e413fd
Dmitriy Naymov <prp4@bk.ru>
2023-08-26 20:20:12 +0300

    prepared tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+
+
+
+
+def test_apply_discount():
+    item1.pay_rate = 0.5
+    assert item1.apply_discount() == 5000.0
