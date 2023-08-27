dd70f096e3de2a24d4095b4ae7c0de1a887d0678
Dmitriy Naymov <prp4@bk.ru>
2023-08-27 22:42:04 +0300

    add all code in item

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import os, csv
+from pathlib import Path
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +17,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
+
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
@@ -30,5 +35,25 @@ class Item:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price *=self.pay_rate
-        return self.price
+        self.price *= self.pay_rate
+
+
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, name):
+        self.__name = name[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        with open(os.path.join('..', path), newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                # item = cls(row['name'], row['price'], row['quantity'])
+                # cls.all.append(item)
+                cls.all.append(cls(row['name'], row['price'], row['quantity']))
+
+    @staticmethod
+    def string_to_number(number):
+        return int(float(number)//1)
